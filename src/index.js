9c3e934af761a8f4b108ef344a91a0f9dee86200
mbukhman <mbukhman@gmail.com>
2022-04-07 01:06:06 +0400

    update

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+import "/assets/style.css";
+
+class EasyQuizy extends Component {
+    render() {
+        return(
+            <div className="container">
+                <div className="title">EasyQuizy</div>
+            </div>
+        );
+    }
+}
